b3562599c43dba1a125d8c005137e9f6c5c346b1
Vinicius Jacques <vjacques@smartcloudinc.com>
2017-04-04 01:57:46 -0300

    Add project files.

---
+++ Jacques.WebToSQL/DataModel.fs
@@ -0,0 +1,35 @@
+﻿namespace Jacques.WebToSql.DataModel
+
+open Microsoft.EntityFrameworkCore
+
+type Apartamento() =
+    member val ID = 0 with get,set
+    member val Valor = 0.f with get,set
+    member val Quartos = 0 with get,set
+    member val Suites = 0 with get,set
+    member val Area_Util = 0 with get,set
+    member val Area_Total = 0 with get,set
+    member val Vagas = 0 with get,set
+    member val Condominio = 0 with get,set
+    member val IPTU = 0.f with get,set
+    member val Bairro = "" with get,set
+    member val Rua = "" with get,set
+    member val Cidade = "" with get,set
+    member val Fotos = 0 with get,set
+    member val Descricao = "" with get,set
+    member val Ano_Construcao = 0 with get,set
+    member val Caracteristicas_Imovel = "" with get,set
+    member val Caracteristicas_Areas_Comuns = "" with get,set
+    member val Andares = 0 with get,set
+
+type EntityContext() =
+    inherit DbContext()
+
+    member x.Apartamentos = x.Set<Apartamento>()
+
+    override x.OnConfiguring(optionsBuilder) =
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;") |> ignore
+
+    override x.OnModelCreating(modelBuilder) =
+        modelBuilder.Entity<Apartamento>().HasKey("ID") |> ignore
+        ()
---
+++ Jacques.WebToSQL/Program.fs
@@ -0,0 +1,33 @@
+﻿open FSharp.Data
+open Jacques.WebToSql.DataModel
+open System.Net
+
+[<EntryPoint>]
+let main argv = 
+    let client = new WebClient()
+    client.Headers.Add("user-agent", "teste")
+
+    let searchUrl = """https://www.zapimoveis.com.br/aluguel/apartamentos/rs+porto-alegre/#{"possuiendereco":"True","parametrosautosuggest":[{"Bairro":"","Zona":"","Cidade":"PORTO ALEGRE","Agrupamento":"","Estado":"RS"}],"pagina":"1","paginaOrigem":"Home","formato":"Lista"}"""
+    
+    let html = client.DownloadString searchUrl |> HtmlDocument.Parse
+    let urlList = [for i in 0..19 do yield ((html.CssSelect ("a#miniFicha" + string i) |> List.head).AttributeValue "href").Replace("https", "http")]
+
+    for url in urlList do
+        printfn "%s" url
+
+    // teste
+    let url = "http://www.zapimoveis.com.br/oferta/aluguel+apartamento+3-quartos+moinhos-de-vento+porto-alegre+rs+105m2+RS2600/ID-9735810/"
+    client.Headers.Add("user-agent", "teste")
+    let resp = client.DownloadString url
+    let html = HtmlDocument.Parse resp
+    let logradouro = (html.CssSelect "div.side-left h1.pull-left span.logradouro" |> List.head).InnerText()
+
+    let ap = new Apartamento()
+    ap.Andares <- 5
+    use db = new EntityContext()
+    db.Database.EnsureDeleted() |> ignore
+    db.Database.EnsureCreated() |> ignore
+    db.Apartamentos.Add(ap) |> ignore
+    db.SaveChanges() |> ignore
+
+    0
