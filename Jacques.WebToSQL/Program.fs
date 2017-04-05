ee4ba46720a1a4e75a90587151026c9bee3d15af
Vinicius Jacques <vjacques@smartcloudinc.com>
2017-04-05 01:09:14 -0300

    refactored asybc. need to load page after JS to parse list contents...

--- Jacques.WebToSQL/Program.fs
+++ Jacques.WebToSQL/Program.fs
@@ -12,28 +12,26 @@ let main argv =
 
     let mutable counter = 0
 
+    let doWork url =
+        async {
+            let! resp = Http.AsyncRequestString(url, headers = [("user-agent","teste")])
+            let ap, id = HtmlToModel resp
+            db.Apartamentos.Add(ap) |> ignore
+            counter <- counter + 1
+            printfn "\t%i ID:%i" counter id
+        }
+
     for i in 1..2 do
         let searchUrl = """https://www.zapimoveis.com.br/aluguel/apartamentos/rs+porto-alegre/#{"possuiendereco":"True","parametrosautosuggest":[{"Bairro":"","Zona":"","Cidade":"PORTO ALEGRE","Agrupamento":"","Estado":"RS"}],"pagina":""" + "\"" + string i + "\"" + ""","paginaOrigem":"Home","formato":"Lista"}"""
         printfn "%i %s" i searchUrl
 
         let content = Http.RequestString(searchUrl, headers = [("user-agent","teste")])
         let html = HtmlDocument.Parse content
-
-        let pagination = html.CssSelect("input#quantidadeTotalPaginas").Head.AttributeValue("data-value") |> float
         let urlList = [for i in 0..19 do yield ((html.CssSelect ("a#miniFicha" + string i) |> List.head).AttributeValue "href").Replace("https", "http")]
 
-        for url in urlList do
-            let task = async {
-                let! resp = Http.AsyncRequestString(url, headers = [("user-agent","teste")])
-                let ap, id = HtmlToModel resp
-                db.Apartamentos.Add(ap) |> ignore
-                counter <- counter + 1
-                printfn "\t%i ID:%i" counter id
-            }
+        urlList |> Seq.map doWork |> Async.Parallel |> Async.RunSynchronously |> ignore
 
-            Async.Start task
+        db.SaveChanges() |> ignore
     
-    Console.ReadLine() |> ignore
-    db.SaveChanges() |> ignore
     Console.ReadLine() |> ignore
     0
